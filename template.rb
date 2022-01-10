b2969381b9559e08cdf0fdf0f7f243d64016642b
Wesley Oliveira <waejloliveira@gmail.com>
2022-01-10 10:31:45 -0300

    Init of project

---
+++ template.rb
@@ -0,0 +1,26 @@
+require 'fileutils'
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'rubocop'
+  gem 'reek'
+  gem 'htmlbeautifier'
+  gem 'shoulda-matchers'
+  gem 'database_cleaner'
+  gem 'factory_bot_rails'
+end
+
+generate "rspec:install"
+
+copy_dir "spec"
+# FileUtils.copy_entry "rspec", 
+
+application do 
+  config.generators do |g|
+    g.javascript false
+    g.stylesheets false
+    g.test_framework :rspec
+    g.test_unit false
+  end
+end
